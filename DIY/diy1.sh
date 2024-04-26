a890bb91845efd7797eecbd491dff724d97ba6ce
mzal <58111339+mzwrt@users.noreply.github.com>
2024-04-26 08:43:22 +0800

    Create diy1.sh

---
+++ DIY/diy1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#=================================================
+# DaoDao's script 
+#=================================================
+##添加自己的插件库
+echo -e "\nsrc-git extraipk https://github.com/mzal188/extra_ipk" >> feeds.conf.default
