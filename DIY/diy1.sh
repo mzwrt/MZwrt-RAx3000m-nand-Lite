2a9f53275a7d0778218b953a7ad26b7d797f94ee
mzal <58111339+mzwrt@users.noreply.github.com>
2024-04-26 09:07:36 +0800

    Update diy1.sh

--- DIY/diy1.sh
+++ DIY/diy1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #=================================================
-# DaoDao's script 
-#=================================================
+# MZwrt script
+#================================================= 
+
+
 ##添加自己的插件库
 echo -e "\nsrc-git extraipk https://github.com/mzal188/extra_ipk" >> feeds.conf.default
