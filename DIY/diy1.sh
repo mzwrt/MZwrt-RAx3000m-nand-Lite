8769d9ab52209662ef04483a2af2ad308794e85a
mzal <58111339+mzwrt@users.noreply.github.com>
2024-12-03 18:52:08 +0800

    Update diy1.sh

--- DIY/diy1.sh
+++ DIY/diy1.sh
@@ -5,4 +5,4 @@
 
 
 ##添加自己的插件库
-echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package" >> feeds.conf.default
+echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
